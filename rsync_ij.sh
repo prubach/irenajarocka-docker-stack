a30fb23ed7ae1b784e9dd5b48ccdeb80b2bbc912
Pawel Rubach <pawel.rubach@gmail.com>
2024-12-24 15:14:20 +0100

    adding locally modified files

--- rsync_ij.sh
+++ rsync_ij.sh
@@ -4,7 +4,8 @@ LOCAL_STACK=/Users/pol/irenajarocka-docker-stack
 LOG_DIR=${LOCAL_STACK}/logs/
 CUR_DATE=`date "+%Y-%m-%d";`
 LOG_FILE=${LOG_DIR}/rsync_${CUR_DATE}.log
-rsync -chavzP --iconv=utf-8-mac,utf-8 -e "/usr/bin/ssh -p 22222" irenajarocka@irenajarocka.pl:/home/irenajarocka/public_html ${LOCAL_DIR} > ${LOG_FILE}
+rsync -chavzP -e "/usr/bin/ssh -p 22222" irenajarocka@irenajarocka.pl:/home/irenajarocka/public_html ${LOCAL_DIR} > ${LOG_FILE}
+#--iconv=utf-8-mac,utf-8
 # copy local config
 #cp -f ${LOCAL_STACK}/secrets/config.php ${LOCAL_DIR}/public_html/lib/
 cp -f ${LOCAL_STACK}/secrets/config.php ${LOCAL_DIR}/lib/
